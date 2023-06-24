5aea8e9edcaddeac6dfb27b17b8a564533774ec5
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-24 15:44:06 +0530

    read file from fs module

--- basicHttpServer/index.js
+++ basicHttpServer/index.js
@@ -1,7 +1,25 @@
 const http = require("http");
 const port = 8000;
 
-const server = http.createServer();
+// require fs module for reading and writing
+const fs = require("fs");
+
+// create a requestHandler
+function requestHandler(req, res) {
+  console.log(req.url);
+  res.writeHead(200, { "content-type": "text/html" });
+
+  // read the file
+  fs.readFile("./basicHttpServer/index.html", function (err, data) {
+    if (err) {
+      console.log("error", err);
+      return res.end(`<h1>Error</h1>`);
+    }
+    return res.end(data);
+  });
+}
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
